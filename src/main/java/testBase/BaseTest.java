f716df72befcaa7ae8be0c9dfc170e5e2086daa7
hema <hemajayanthreddy@gmail.com>
2025-02-03 00:27:41 -0600

    remove unwanted imports for after and before methods

--- src/main/java/testBase/BaseTest.java
+++ src/main/java/testBase/BaseTest.java
@@ -11,10 +11,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 
-import pageObjects.LandingPage;
 
 
 public class BaseTest {
