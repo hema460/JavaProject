8176841c6b7a1591546cf31760df79e9885b73df
hema <hemajayanthreddy@gmail.com>
2025-02-25 23:31:29 -0600

    made some changes to TestRunner class

--- src/test/java/myTestRunner/TestRunner.java
+++ src/test/java/myTestRunner/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/java/bddcucumber",
         glue="stepDefinitions",monochrome=true,dryRun=false,
-        plugin= {"pretty","json:target/cucumber.json", "html:target/cucumber-reports" })
+        plugin= {"pretty","json:target/cucumber.json", "html:target/cucumber-report-html/cucumber-html-reports" })
         
                        
 public class TestRunner extends AbstractTestNGCucumberTests{
