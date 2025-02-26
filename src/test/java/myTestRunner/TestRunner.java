f4e904b51c641ba459f16a7ed6ac6f32cd027363
hema <hemajayanthreddy@gmail.com>
2025-02-26 01:48:48 -0600

    added feature file under src/test/resources instead of src/test/java/bddcucumber

--- src/test/java/myTestRunner/TestRunner.java
+++ src/test/java/myTestRunner/TestRunner.java
@@ -3,7 +3,7 @@ package myTestRunner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/java/bddcucumber",
+@CucumberOptions(features = "src/test/resources",
         glue="stepDefinitions",monochrome=true,dryRun=false,
         plugin= {"pretty","json:target/cucumber.json", "html:target/cucumber-report-html/cucumber-html-reports" })
         
